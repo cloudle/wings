26a3a9a6c8134d4f4c0d27adf04e7dffc1155fb5
cloudle <lehaoson@gmail.com>
2020-07-06 17:23:07 +0700

    hot-reload node.js server (and related modules)

--- playground/index.node.js
+++ playground/index.node.js
@@ -1,9 +1,20 @@
 console.log('this is Node!!');
 
-export const configure = async (server, express) => {
+export const configureServer = async (server, express) => {
 	server.use('/api', (req, res) => {
 		res.json({ message: 'hello!' });
 	});
 
 	return server;
+};
+
+export const configureRouter = (server, express) => {
+	const { Router, } = express;
+
+	const router = Router();
+	router.use('/ap', (req, res) => {
+		res.json({ message: 'hello world' });
+	});
+
+	return router;
 };
--- src/utils/server/node.js
+++ src/utils/server/node.js
@@ -1,5 +1,9 @@
+import path from 'path';
+import chokidar from 'chokidar';
+import invalidate from 'invalidate-module';
+import moduleAlias from 'module-alias';
+import chalk from 'chalk';
 import { extractGlobalModules, } from '../modules';
-import { configure } from '../../../playground/index.node';
 
 const globalModules = extractGlobalModules();
 const { wingsConfig, wingsHelper, webpack, express, } = globalModules;
@@ -12,10 +16,30 @@ const port = wingsConfig.ssrPort();
 const server = express();
 const nodeEntry = requireModule('index.node.js');
 const defaultServerConfigure = () => new Promise((resolve, reject) => resolve());
-const serverConfigure = nodeEntry.configure || defaultServerConfigure;
+const serverConfigure = nodeEntry.configureServer || defaultServerConfigure;
+
+moduleAlias.addAlias('react-native', 'react-native-web');
+
+if (!isProduction) { /* <- hot reload server-side code on development mode */
+	const watcher = chokidar.watch([
+		path.resolve(process.cwd(), './src'),
+		path.resolve(process.cwd(), './index.js'),
+		path.resolve(process.cwd(), './index.node.js'),
+	], { ignoreInitial: true });
+
+	watcher.on('all', (event, filename) => {
+		console.log(chalk.magenta('hot code reload'), chalk.green(filename), 'updated.');
+		invalidate(path.resolve(filename));
+	});
+}
 
 server.set('view engine', 'ejs');
 server.use(express.static(staticPath));
+
+server.use((req, res, next) => {
+	require(path.resolve(process.cwd(), './index.node.js'))
+		.configureRouter(server, express)(req, res, next);
+});
 
 serverConfigure(server).then(() => {
 	server.listen(port, host, () => {
