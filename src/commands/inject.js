95bd92af27de86ad9b25f540966d4cafc25ee5e6
cloudle <lehaoson@gmail.com>
2020-07-09 17:40:13 +0700

    rename boilerplate, added lambda/bare

--- src/commands/inject.js
+++ src/commands/inject.js
@@ -1,18 +1,10 @@
+import { walk, } from '../utils/fileProcessing';
+
 export default {
 	command: 'inject',
 	desc: 'add [boilerplate|template] to existing project',
 	// usage: 'usage: $0 create <item> [options]',
-	builder: (yargs) => {
-		const argv = yargs.command({
-			command: 'project',
-			desc: 'inject a project',
-			handler: () => {
-				console.log('inner command');
-			},
-		}).wrap(null).argv;
-
-		if (argv._.length < 2) {
-			yargs.showHelp();
-		}
+	handler: (args) => {
+		console.log(walk(__dirname));
 	},
 };
